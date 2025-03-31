72e74c2d4734f84c60f7023c8599ba858ee6ca92
OKH@nd.ZY <OKHand.ziyu@gmail.com>
2025-03-31 14:50:39 +0800

    Other: 🌈 filter doc

--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
             <div className="navbar-content">
                 <div className="navbar-logo">RF Studio</div>
                 <div className="navbar-links">
-                    <a href="https://github.com/your-repo" target="_blank" rel="noopener noreferrer">GitHub</a>
+                    <a href="https://github.com/OKHand-Zy/RFStudio" target="_blank" rel="noopener noreferrer">GitHub Repo</a>
                     <a href="https://robotframework.org/" target="_blank" rel="noopener noreferrer">Robot Framework</a>
                     <a href="https://robotframework.org/robotframework/" target="_blank" rel="noopener noreferrer">Robot Framework Doc</a>
                 </div>
